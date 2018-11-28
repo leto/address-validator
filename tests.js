c95590ec8806c0f6a8c4419f898244fe3c1d0ca9
Jonathan "Duke" Leto <jonathan@leto.net>
2018-11-28 15:17:00 -0800

    add some more tests

--- tests.js
+++ tests.js
@@ -1,6 +1,8 @@
 
 QUnit.test( "Basic invalid addresses which have correct prefixes", function( assert ) {
   assert.notOk( isValidAddress("BTC", "1invalid") , "Passed!" );
+  assert.notOk( isValidAddress("ETH", "0xdeadbeef") , "Passed!" );
+  assert.notOk( isValidAddress("ETC", "0xdeadbeef") , "Passed!" );
   assert.notOk( isValidAddress("ZEC", "t1invalid") , "Passed!" );
   assert.notOk( isValidAddress("HUSH", "t1invalid") , "Passed!" );
 });
@@ -8,6 +10,7 @@ QUnit.test( "Basic invalid addresses which have correct prefixes", function( ass
 QUnit.test( "Basic valid addresses", function( assert ) {
   assert.ok( isValidAddress("PIVX", "DMkYTxdS4a6gWqxcDy4xkNcSAbFpWR9T54") , "Passed!" );
   assert.ok( isValidAddress("DASH", "XMkYTxdS4a6gWqxcDy4xkNcSAbFpWR9T54") , "Passed!" );
+  assert.ok( isValidAddress("LTC", "LMkYTxdS4a6gWqxcDy4xkNcSAbFpWR9T54") , "Passed!" );
   assert.ok( isValidAddress("BTG", "GMkYTxdS4a6gWqxcDy4xkNcSAbFpWR9T54") , "Passed!" );
   assert.ok( isValidAddress("BTG", "AMkYTxdS4a6gWqxcDy4xkNcSAbFpWR9T54") , "Passed!" );
 });
